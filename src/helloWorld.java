3df85d5c14f099ba141ef04141ba12fe4dcad3c9
aditya 1031 <Adityasingh1031@gmail.com>
2026-04-14 11:21:36 +0530

    Initial commit

---
+++ src/helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("my name is aditya kumar");
+        System.out.println(3 + 6);
+    }
+}
+
+
